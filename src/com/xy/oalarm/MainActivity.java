a282a576a761dfc94f0cb63ca0c6ba8768f0634d
Laci <yx0118@gmail.com>
2014-02-22 17:13:23 +0800

    add alarm 	modified:   README.md 	modified:   res/layout/activity_main.xml 	modified:   src/com/xy/oalarm/MainActivity.java

--- src/com/xy/oalarm/MainActivity.java
+++ src/com/xy/oalarm/MainActivity.java
@@ -4,14 +4,27 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
+	private Button mTestAlarm;
+
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		startService(new Intent(MainActivity.this, AlarmService.class));
+		mTestAlarm = (Button)findViewById(R.id.startAlarm);
+		mTestAlarm.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View arg0) {
+				// TODO Auto-generated method stub
+				sendBroadcast(new Intent(Constant.ACTION_ALARM));
+			}
+		});
 	}
 
 	@Override
